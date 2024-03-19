d0b7bd0d249605ddf1c219a9f7149760b695ba34
Colt Frederickson <coltfred@gmail.com>
2024-03-19 12:34:54 -0600

    Update instructions and change script to work with just local testing (#53)

--- README.md
+++ README.md
@@ -1,34 +1,29 @@
-# Perf Testing
+# Cloaked Search Performance Testing
+
+This repository is a way to do some basic performance testing of Cloaked Search. This tests the indexing using [rally](https://esrally.readthedocs.io/en/stable/) and query tests using apache bench. We have some [results](./results/) committed and ways to run them locally and in our k8s cluster.
+
+Note that results will vary wildly depending on your os, cpu, memory, etc. We're only really looking for if the results are wildly different when running them in k8s since we more tightly control the resources there.
 
 ## Building docker container
 
 The Dockerfile in this repo is a thin wrapper around the esrally one that just adds Apache Bench to the container. In order to build it I did the following:
 
 ```bash
-docker build . -t IronCoreLabs/rally:2.7.1
-docker tag docker.io/ironcorelabs/rally:2.7.1  gcr.io/ironcore-dev-1/rally:2.7.1
-docker push gcr.io/ironcore-dev-1/rally:2.7.1
+docker build . -t ironcorelabs/rally:2.7.1
 ```
 
 The version of the container should change if the Dockerfile has a different version of the rally tool inside of it.
 
-## Local setup
-
-First, decrypt the keys.
+If you're building a new version for testing using k8s, you then want to tag and push it to our private repository. For local testing this isn't required.
 
 ```bash
-ironhide file decrypt cs-config/test.key.iron -o cs-config/test.key
+docker tag docker.io/ironcorelabs/rally:2.7.1  gcr.io/ironcore-dev-1/rally:2.7.1
+docker push gcr.io/ironcore-dev-1/rally:2.7.1
 ```
 
-```bash
-ironhide file decrypt tsp-config/tsp.env.iron -o tsp-config/tsp.env
-```
+## Local testing
 
-```bash
-ironhide file decrypt test-runner.env.iron -o test-runner.env
-```
-
-Start up the services:
+Start up the services and run the test:
 
 ```bash
 docker compose up
@@ -36,22 +31,12 @@ docker compose up
 
 Note that this also binds to the local port 9200 in case you want to poke at the container. If you have a local ES instance you might want to remove the port bindings.
 
-Run rally against cs, repeat each time you want to run it:
+The results will be in `rally-home/cloaked-search-perf/results`.
+
+If you want to run them again you can:
 
 ```bash
-docker compose run test-runner race --track-path=/tracks/so500k --test-mode --pipeline=benchmark-only --target-hosts=cs:8675
-```
-
-You can also run rally against cs, es, create a diff and open a pull request if you run the `run-500k.sh`.
-
-```bash
-docker compose run test-runner /app/run-500k.sh
-```
-
-If you want to restart a service (`cs`, `es`, `tsp`, or `test-runner`):
-
-```bash
-docker compose restart [SERVICE]
+docker compose restart test-runner
 ```
 
 If you make changes to the `docker-compose.yml` (for ex a new CS image) and want to load them without restarting everything:
--- run-500k.sh
+++ run-500k.sh
@@ -11,24 +11,30 @@ CURRENT_DATE=$(date +%F)
 BRANCH_NAME="$CURRENT_DATE-$UUID"
 QUERY_CONCURRENCY=5
 QUERY_TOTAL_PER_QUERY=500
+GIT_URL=https://github.com/IronCoreLabs/cloaked-search-perf.git
+EMAIL="default@example.com"
+USERNAME=default
 
 [[ -z "${CS_HOST}" ]] && CS_HOST='cs:8675' && echo "Defaulting CS host"
 [[ -z "${ES_HOST}" ]] && ES_HOST='es:9200' && echo "Defaulting ES host"
 
 #Check to see if we have all the env variables we need
 if [ ! -v PAT ]; then
-  echo "PAT must be set in the env. It should be a personal access token which is able to clone and pull request to cloaked-search-perf."
-  exit 1
+  echo "PAT is not set in the env. It should be a personal access token which is able to clone and pull request to cloaked-search-perf if you want to report your results."
+else
+  GIT_URL="https://$PAT@github.com/IronCoreLabs/cloaked-search-perf.git"
 fi
 
 if [ ! -v PAT_USER_NAME ]; then
-  echo "PAT_USER_NAME must be set in the env."
-  exit 1
+  echo "PAT_USER_NAME is not set in the env."
+else
+  USERNAME="$PAT_USER_NAME"
 fi
 
 if [ ! -v PAT_EMAIL ]; then
-  echo "PAT_EMAIL must be set in the env."
-  exit 1
+  echo "PAT_EMAIL is not set in the env."
+else
+  EMAIL="$PAT_EMAIL"
 fi
 
 if [ ! -d /rally/.rally ]; then
@@ -36,21 +42,22 @@ if [ ! -d /rally/.rally ]; then
   exit 1
 fi
 
-cd /rally/.rally
 
-if [ ! -d cloaked-search-perf ]; then
-  git clone "https://$PAT@github.com/IronCoreLabs/cloaked-search-perf.git"
-fi
+while ! curl -kSsf "${CS_HOST}/_cloaked_search/health" ; do
+  sleep 10
+done
+
+
+cd /rally/.rally
+# We want to start with a clean slate since having a PAT and not can change from run to run.
+rm -rf cloaked-search-perf
+git clone "$GIT_URL"
 
 # Go into the git repo, make sure it's clean and create a branch
 cd cloaked-search-perf
 git config pull.rebase true
-git config user.email "$PAT_EMAIL"
-git config user.name "$PAT_USER_NAME"
-git checkout main
-git reset --hard
-git clean -f
-git pull origin main
+git config user.email "$EMAIL"
+git config user.name "$USERNAME"
 
 OUTPUT_DIR="/rally/.rally/cloaked-search-perf/results/${CURRENT_DATE}/$UUID"
 git checkout -b "$BRANCH_NAME"
@@ -118,14 +125,18 @@ query_all "$CS_HOST" "/app/queries" "$CS_IDENT"
 # Run the compare utility
 esrally compare "--baseline=$ES_IDENT" "--contender=$CS_IDENT" --report-format=csv "--report-file=$OUTPUT_DIR/compare-$SECONDS_SINCE_EPOCH.csv"
 
-git add "$OUTPUT_DIR"
-git commit -m "Perf test run $UUID."
-git push origin "$BRANCH_NAME"
 
-curl -sSf \
-  -X POST \
-  -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer $PAT"\
-  -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/IronCoreLabs/cloaked-search-perf/pulls \
-  -d "{\"title\":\"$CURRENT_DATE - Performance test results\",\"body\":\"These are the results from run $UUID\",\"head\":\"$BRANCH_NAME\",\"base\":\"main\"}"
+# If the PAT is set, we cloned via the URL to be able to push this branch, so add the run and push it.
+if [ -v PAT ]; then
+  git add "$OUTPUT_DIR"
+  git commit -m "Perf test run $UUID."
+  git push origin "$BRANCH_NAME"
+
+  curl -sSf \
+    -X POST \
+    -H "Accept: application/vnd.github+json" \
+    -H "Authorization: Bearer $PAT"\
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    https://api.github.com/repos/IronCoreLabs/cloaked-search-perf/pulls \
+    -d "{\"title\":\"$CURRENT_DATE - Performance test results\",\"body\":\"These are the results from run $UUID\",\"head\":\"$BRANCH_NAME\",\"base\":\"main\"}"
+fi
